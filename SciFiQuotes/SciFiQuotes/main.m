95b34be09042327496f31033a7c47ccca13f88da
Xavi Moll <xmollv@gmail.com>
2018-04-30 11:12:26 +0200

    Started the second project

---
+++ SciFiQuotes/SciFiQuotes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SciFiQuotes
+//
+//  Created by Xavi Moll on 30/04/2018.
+//  Copyright © 2018 xmollv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
